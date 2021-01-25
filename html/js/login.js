9726a34e232672c7c53fe4deb7441e1f2e689b07
Christian Mayo <mayo7chris@gmail.com>
2021-01-25 03:54:45 -0500

    Adds the login.js file that is untested

---
+++ html/js/login.js
@@ -0,0 +1,176 @@
+let urlBase = 'http://www.cop4331-group2.com/PHP/api';
+let extension = 'php';
+
+let userId = 0;
+let firstName = "";
+let lastName = "";
+
+function doLogin()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	
+	let login = document.getElementById("email").value;
+	let password = document.getElementById("password").value;
+//	let hash = md5( password );
+	
+	document.getElementById("loginResult").innerHTML = "";
+
+//	let jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
+	let jsonPayload = '{"email" : "' + login + '", "password" : "' + password + '"}';
+	let url = urlBase + '/Login.' + extension;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, false);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.send(jsonPayload);
+		
+		let jsonObject = JSON.parse( xhr.responseText );
+		
+		userId = jsonObject.id;
+		
+		if( userId < 1 )
+		{
+			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+			return;
+		}
+		
+		firstName = jsonObject.firstName;
+		lastName = jsonObject.lastName;
+
+		saveCookie(); 
+	
+		window.location.href = "home.html";
+	}
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
+
+}
+
+function saveCookie()
+{
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime() + (minutes * 60 * 1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	let data = document.cookie;
+	let splits = data.split(",");
+
+	for(let i = 0; i < splits.length; i++) 
+	{
+		let thisOne = splits[i].trim();
+		let tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	}
+}
+
+function doLogout()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "index.html";
+}
+
+// function addColor()
+// {
+// 	let newColor = document.getElementById("colorText").value;
+// 	document.getElementById("colorAddResult").innerHTML = "";
+	
+// 	let jsonPayload = '{"color" : "' + newColor + '", "userId" : ' + userId + '}';
+// 	let url = urlBase + '/AddColor.' + extension;
+	
+// 	let xhr = new XMLHttpRequest();
+// 	xhr.open("POST", url, true);
+// 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+// 	try
+// 	{
+// 		xhr.onreadystatechange = function() 
+// 		{
+// 			if (this.readyState == 4 && this.status == 200) 
+// 			{
+// 				document.getElementById("colorAddResult").innerHTML = "Color has been added";
+// 			}
+// 		};
+// 		xhr.send(jsonPayload);
+// 	}
+// 	catch(err)
+// 	{
+// 		document.getElementById("colorAddResult").innerHTML = err.message;
+// 	}
+	
+// }
+
+// function searchColor()
+// {
+// 	let srch = document.getElementById("searchText").value;
+// 	document.getElementById("colorSearchResult").innerHTML = "";
+	
+// 	let colorList = "";
+	
+// 	let jsonPayload = '{"search" : "' + srch + '","userId" : ' + userId + '}';
+// 	let url = urlBase + '/SearchColors.' + extension;
+	
+// 	let xhr = new XMLHttpRequest();
+// 	xhr.open("POST", url, true);
+// 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+// 	try
+// 	{
+// 		xhr.onreadystatechange = function() 
+// 		{
+// 			if (this.readyState == 4 && this.status == 200) 
+// 			{
+// 				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
+// 				let jsonObject = JSON.parse( xhr.responseText );
+				
+// 				for( let i=0; i<jsonObject.results.length; i++ )
+// 				{
+// 					colorList += jsonObject.results[i];
+// 					if( i < jsonObject.results.length - 1 )
+// 					{
+// 						colorList += "<br />\r\n";
+// 					}
+// 				}
+				
+// 				document.getElementsByTagName("p")[0].innerHTML = colorList;
+// 			}
+// 		};
+// 		xhr.send(jsonPayload);
+// 	}
+// 	catch(err)
+// 	{
+// 		document.getElementById("colorSearchResult").innerHTML = err.message;
+// 	}
+	
+// }
