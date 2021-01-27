6abb0cdda4b1ccbe45c70e8fc427753d4be7416c
Christian Mayo <mayo7chris@gmail.com>
2021-01-26 19:36:52 -0500

    Adds img to signup and adds signup.js

--- html/js/login.js
+++ html/js/login.js
@@ -61,47 +61,6 @@ function doLogin()
 
 }
 
-// function saveCookie()
-// {
-// 	let minutes = 20;
-// 	let date = new Date();
-// 	date.setTime(date.getTime() + (minutes * 60 * 1000));	
-// 	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
-// }
-
-// function readCookie()
-// {
-// 	userId = -1;
-// 	let data = document.cookie;
-// 	let splits = data.split(",");
-
-// 	for(let i = 0; i < splits.length; i++) 
-// 	{
-// 		let thisOne = splits[i].trim();
-// 		let tokens = thisOne.split("=");
-// 		if( tokens[0] == "firstName" )
-// 		{
-// 			firstName = tokens[1];
-// 		}
-// 		else if( tokens[0] == "lastName" )
-// 		{
-// 			lastName = tokens[1];
-// 		}
-// 		else if( tokens[0] == "userId" )
-// 		{
-// 			userId = parseInt( tokens[1].trim() );
-// 		}
-// 	}
-	
-// 	if( userId < 0 )
-// 	{
-// 		window.location.href = "index.html";
-// 	}
-// 	else
-// 	{
-// 		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
-// 	}
-// }
 
 function doLogout()
 {
@@ -109,6 +68,7 @@ function doLogout()
 	firstName = "";
 	lastName = "";
 	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	
 	window.location.href = "index.html";
 }
 
