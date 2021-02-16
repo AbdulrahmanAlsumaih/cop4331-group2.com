0012a1335a981540b07df5987407e846bb504723
Christian Mayo <mayo7chris@gmail.com>
2021-02-15 19:16:21 -0500

    fix attempt 4

--- html/js/login.js
+++ html/js/login.js
@@ -55,3 +55,59 @@ function doLogout() {
 	window.location.href = "index.html";
 }
 
+function doSignUp() {
+	//userId = 0;
+	// firstName = "";
+	// lastName = "";
+	
+	// Getting data from the html IDs
+	let signupEmail = document.getElementById("signupEmail").value;
+	let password = document.getElementById("signupPassword").value;
+	//let hash = md5(password); // Hashing password
+	
+    document.getElementById("signupResult").innerHTML = "";
+    
+    if (signupEmail == "" && signupPassword == "") {
+        document.getElementById("signupResult").innerHTML = "Please enter an email/password";
+        return;
+    }
+
+    if (signupEmail == "") {
+        document.getElementById("signupResult").innerHTML = "Please enter an email address";
+        return;
+    }
+
+    if (signupPassword == "") {
+        document.getElementById("signupResult").innerHTML = "Please enter a password";
+        return;
+    }
+
+	// Creating the jsonPayload
+	let jsonPayload = '{"email" : "' + signupEmail + '", "pass" : "' + password + '"}';
+	let url = urlBase + '/newuser.' + extension;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		// Sending json
+		xhr.send(jsonPayload);
+
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("signupResult").innerHTML = "Account registered!";
+				window.location.href = "home.html";
+			}
+		};
+		
+	}
+	catch(err)
+	{
+		document.getElementById("signupResult").innerHTML = err.message;
+	}
+
+}
+
